7e624dbf68027115e7953213c63757881ff60953
jcastro506 <castrojoshua805.com>
2022-03-24 21:41:36 -0400

    finish details page ui

--- app/src/main/java/com/example/beerinfo/presentation/beer_detail/components/BeerDetailsScreen.kt
+++ app/src/main/java/com/example/beerinfo/presentation/beer_detail/components/BeerDetailsScreen.kt
@@ -1,13 +1,21 @@
 package com.example.beerinfo.presentation.beer_detail.components
 
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.Coil
 import coil.compose.rememberImagePainter
 import com.example.beerinfo.domain.model.Beer
 import com.example.beerinfo.presentation.beer_detail.BeerDetailViewModel
@@ -17,17 +25,8 @@ fun BeerDetailsScreen(
     viewModel : BeerDetailViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    androidx.compose.material.Surface(modifier = Modifier.fillMaxSize()) {
-        state.beer?.let { 
-            LazyColumn() {
-                item {
-                    Text(text = it.name)
-                    Text(text = it.description)
-                    it.foodPairing.map { 
-                        Text(text = it)
-                    }
-                }
-            }
+        state.beer?.let { beer ->
+            Text(beer.name)
         }
-    }
 }
+
